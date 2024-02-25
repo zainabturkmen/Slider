d65fb38fdfc7e24dec97307c5f646add83ecd82d
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-02-25 14:04:37 +0430

    adding some changes

--- app.js
+++ app.js
@@ -19,6 +19,14 @@
  });
 
  function carousel(){
+    // working with slides
+
+    if(counter === slides.length){
+        counter = 0;
+    }
+    if(counter< 0){
+        counter = slides.length -1;
+    }
     slides.forEach(function(slide){
         slide.style.transform = `translateX(-${counter * 100})`
     })
